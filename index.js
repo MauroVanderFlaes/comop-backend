1dcfa9778f2306b657bdf7ca9bbe6ff0bf5333d9
JarneSimons <r0901961@student.thomasmore.be>
2024-03-16 15:35:40 +0100

    feat: adding test routes

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const http = require('node:http');
+const express = require('express')
+const app = express()
+const port = 3000
 
-const hostname = '127.0.0.1';
-const port = 3000;
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+}) 
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
-});
+app.get('/api/v1/challenges', (req, res) => {res.send('This are the challenges')})
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-}); 
+app.put('/api/v1/challenges/:id', (req, res) => {res.send('UPDATE CHALLENGES' + req.params.id)})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
