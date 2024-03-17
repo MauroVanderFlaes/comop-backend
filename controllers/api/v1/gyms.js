0346a66edcb38b0cca6242d9e23dcc294922153b
JarneSimons <r0901961@student.thomasmore.be>
2024-03-17 19:14:40 +0100

    feat: qr code scan works

--- controllers/api/v1/gyms.js
+++ controllers/api/v1/gyms.js
@@ -1,36 +1,32 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const gymSchema = new Schema({
-    name: String,
-    adress: String,
-    city: String,
-    qrCode: String
-})
-const Gyms = mongoose.model('Gyms', gymSchema);
+const Gyms = require('../../../models/Gyms');
 
-const getGyms = (req, res) => {
-    res.json({
-        gyms: [
-            {
-                name: "Gym 1",
-                adress: "123 Main St",
-                city: "Denver",
-                state: "CO",
-                zip: "80202"
-            },
-        ]
-    })
+const getGyms = async (req, res) => {
+    try {
+        const gyms = await Gyms.find();
+        res.json({
+            "status": "success",
+            "data": gyms
+        });
+    } catch (error) {
+        res.json({
+            "status": "error",
+            "message": error.message
+        });
+    }
 }
 
 const postGyms = async (req, res) => {
     try {
         let { name, address, city, qrCode } = req.body;
+
+        const imageData = Buffer.from(req.body.imageData, 'base64');
         
         let gyms = new Gyms({
             name,
             address,
             city,
-            qrCode
+            qrCode,
+            imageData: imageData
         });
 
         const savedGym = await gyms.save();
@@ -47,5 +43,45 @@ const postGyms = async (req, res) => {
     }
 }
 
+const compareQrCode = async (req, res) => {
+    try {
+        
+        const scannedQrCode = req.body.qrCode;
+        
+
+        const gyms = await Gyms.find();
+
+        
+        gyms.forEach(gym => {
+            // console.log("QR code", gym.qrCode);
+        });
+
+        // console.log("this is the scanned qr code", scannedQrCode);
+        // console.log("this is the qr code", qrCode);
+
+        const matchingGym = gyms.find(gym => gym.qrCode === scannedQrCode);
+
+
+        if (matchingGym) {
+            console.log("Matching gym found", matchingGym);
+            res.json({
+                "status": "success",
+                "data": matchingGym
+            });
+        } else {
+            res.json({
+                "status": "error",
+                "message": "No matching gym found"
+            });
+        }
+    } catch (error) {
+        res.json({
+            "status": "error",
+            "message": error.message
+        });
+    }
+}
+
 module.exports.getGyms = getGyms;
 module.exports.postGyms = postGyms;
+module.exports.compareQrCode = compareQrCode;
