c97b586990727b25e47f00df3eae1964cfde2060
JarneSimons <r0901961@student.thomasmore.be>
2024-04-07 21:29:51 +0200

    feat: get data from gym

--- routes/api/v1/gyms.js
+++ routes/api/v1/gyms.js
@@ -7,5 +7,8 @@ const gymsController = require('../../../controllers/api/v1/gyms')
 router.get('/', gymsController.getGyms);
 router.post('/', gymsController.postGyms);
 router.post('/compareQrCode', gymsController.compareQrCode);
+router.get('/compareQrCode/:qrCode', gymsController.getGymByQrCode);
+// router.get('/compareQrCode', gymsController.getGymByQrCode)
+
 
 module.exports = router
