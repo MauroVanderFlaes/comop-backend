616099ab1ecb9d08ed66bc4d1eeeb4eb03847bac
JarneSimons <r0901961@student.thomasmore.be>
2024-06-11 00:26:08 +0200

    feat: challenge start and cancel

--- routes/api/v1/challenges.js
+++ routes/api/v1/challenges.js
@@ -5,5 +5,7 @@ const challengesController = require('../../../controllers/api/v1/challenges');
 router.post('/', challengesController.createChallenges);
 router.get('/', challengesController.getChallenges);
 router.put('/:id', challengesController.updateChallenges);
+router.put('/active/:id', challengesController.toggleActive);
+router.get('/active', challengesController.getActiveChallenges);
 
 module.exports = router;
